e21f3819e809f2ef337f8361106732bc7dfeac26
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2024-08-26 11:48:25 +0545

    Add feature of form when clicked on the "Add book" btn

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ function Book(title, author, pages, readStatus) {
 function addBookToLibrary(title, author, pages, readStatus) {
     myLibrary.push(new Book(title, author, pages, readStatus))
 }
+
+function openBookForm() {
+    library.parentNode.insertBefore(bookForm, library);
+}
+
+const library = document.querySelector('.library')
+const bookContainer = document.querySelector('.book-container');
+const bookForm = document.querySelector('.book-form');
+bookForm.remove();
+const addBookButton = document.querySelector('.library > .addBook');
+
+
+addBookButton.addEventListener('click', openBookForm);
