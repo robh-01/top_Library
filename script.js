0c38b0803e31e32a46fc0ddd1fadcd9f46387051
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2024-08-26 13:30:45 +0545

    Add function that pushes 'Book Object' to the 'myLibrary Array'
    This commit add the functionality due to which when the add button of the FORM is clicked, the book object with the value entered by the user is added to the myLibrary array

--- script.js
+++ script.js
@@ -8,18 +8,32 @@ function Book(title, author, pages, readStatus) {
 }
 
 function addBookToLibrary(title, author, pages, readStatus) {
-    myLibrary.push(new Book(title, author, pages, readStatus))
+    myLibrary.push(new Book(title, author, pages, readStatus));
 }
 
 function openBookForm() {
     library.parentNode.insertBefore(bookForm, library);
 }
 
-const library = document.querySelector('.library')
+const library = document.querySelector('.library');
 const bookContainer = document.querySelector('.book-container');
 const bookForm = document.querySelector('.book-form');
+const addToLibraryBtn = bookForm.querySelector('button');
+const bookTitleInput = bookForm.querySelector('*[id="title"]');
+const bookAuthorInput = bookForm.querySelector('*[id="author"]');
+const bookPagesNumberInput = bookForm.querySelector('*[id="pages"]');
+const bookReadStatusInput = bookForm.querySelectorAll('*[name="readStatus"]');
 bookForm.remove();
 const addBookButton = document.querySelector('.library > .addBook');
 
 
 addBookButton.addEventListener('click', openBookForm);
+addToLibraryBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let readStatus
+    if(bookReadStatusInput[0].checked){
+        readStatus = +bookReadStatusInput[0].value;
+    }
+    else readStatus = +bookReadStatusInput[1].value;
+    addBookToLibrary(bookTitleInput.value, bookAuthorInput.value, bookPagesNumberInput.value, readStatus);
+})
